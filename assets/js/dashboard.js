b3a37e441f31b2673e723e1e4c4f7cc29ed407f0
clyerica <cly.erica@gmail.com>
2022-06-09 10:11:23 +0800

    synced drc charts

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,13 +1,22 @@
 const drcAreaChart=new ApexCharts(
     document.querySelector('#drc-area-chart'), {
         'chart':{
+            'id':'drc-total',
             'type':'area',
             'height':'100%',
             'stacked':true,
-            'group':'drc-charts'
+            'group':'drc-charts',
         },
         'series': [],
         'noData': { 'text': 'please wait, data is loading' },
+        'xaxis':{
+            'type':'categories'
+        },
+        'yaxis':{
+            'labels':{
+                'minWidth':20
+            }
+        }
     }
 )
 
@@ -25,13 +34,13 @@ async function loadAreaChartData(){
     }
 
     for (let dataPoint of json1){
-        series["inmatePopulation"].push({
+        series.inmatePopulation.push({
             'x':dataPoint.year,
             'y':dataPoint.number_of_population
         })
     }
     for (let dataPoint of json2){
-        series["releasedInmates"].push({
+        series.releasedInmates.push({
             'x':dataPoint.year,
             'y':dataPoint.number_of_releases
         })
@@ -43,26 +52,92 @@ async function loadAreaChartData(){
 const drcPopulationChart= new ApexCharts(
     document.querySelector('#drc-population'),{
         'chart':{
+            'id':'drc-population',
             'type':'line',
             'height':'100%',
             'group':'drc-charts'
         },
         'series': [],
         'noData': { 'text': 'please wait, data is loading' },
+        'xaxis':{
+            'type':'categories'
+        },
+        'yaxis':{
+            'labels':{
+                'minWidth':20
+            }
+        }
     }
 )
 
 const drcReleasedChart= new ApexCharts(
     document.querySelector('#drc-released'),{
         'chart':{
+            'id':'drc-released',
             'type':'line',
             'height':'100%',
             'group':'drc-charts'
         },
         'series': [],
         'noData': { 'text': 'please wait, data is loading' },
+        'xaxis':{
+            'type':'categories'
+        },
+        'yaxis':{
+            'labels':{
+                'minWidth':20
+            }
+        }
     }
 )
+
+async function loadPopulationChartData(){
+    const response=await axios.get('assets/data/drc-population-gender.csv')
+    const json=await csv().fromString(response.data)
+    let series={
+        'male':[],
+        'female':[],
+    }
+    for (let dataPoint of json){
+        if (dataPoint.population_by_gender=='Male'){
+            series.male.push({
+                'x': dataPoint.year,
+                'y':dataPoint.number_of_population
+            })
+        }
+        else {
+            series.female.push({
+                'x': dataPoint.year,
+                'y':dataPoint.number_of_population
+            })
+        }
+    }
+    return series
+}
+
+async function loadReleasedChartData(){
+    const response=await axios.get('assets/data/drc-released-gender.csv')
+    const json=await csv().fromString(response.data)
+    let series={
+        'male':[],
+        'female':[],
+    }
+    for (let dataPoint of json){
+        if (dataPoint.releases_by_gender=='Male'){
+            series.male.push({
+                'x': dataPoint.year,
+                'y':dataPoint.number_of_releases
+            })
+        }
+        else {
+            series.female.push({
+                'x': dataPoint.year,
+                'y':dataPoint.number_of_releases
+            })
+        }
+    }
+    return series
+}
 
 drcAreaChart.render();
 drcPopulationChart.render();
@@ -70,6 +145,9 @@ drcReleasedChart.render();
 
 window.addEventListener('DOMContentLoaded', async function () {
     let data = await loadAreaChartData()
+    let populationData=await loadPopulationChartData()
+    let releasedData=await loadReleasedChartData()
+
     drcAreaChart.updateSeries(
         [{
                 'name': 'Population of inmates',
@@ -80,4 +158,30 @@ window.addEventListener('DOMContentLoaded', async function () {
                 'data': data.releasedInmates
             } ]
     )
+    
+    drcPopulationChart.updateSeries(
+        [{
+            'name':'Male',
+            'data': populationData.male
+        },
+        {
+            'name':'Female',
+            'data':populationData.female
+        }
+    ]
+    )
+
+    drcReleasedChart.updateSeries(
+        [{
+            'name':'Male',
+            'data': releasedData.male
+        },
+        {
+            'name':'Female',
+            'data':releasedData.female
+        }
+    ]
+    )
     })
+   
+

1d8c24273a4b06348c6d23fe953097cf6a5e5d75
clyerica <cly.erica@gmail.com>
2022-06-22 10:43:41 +0800

    updated mail-form validation to work across pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,36 @@
-function extractMailFormValues(){
-    let firstName=document.querySelector('#first-name').value;
-    let lastName=document.querySelector('#last-name').value;
-    let email=document.querySelector('#email').value;
-    let formInfo={
-        'firstName':firstName,
-        'lastName':lastName,
-        'email':email
+function extractMailFormValues() {
+    let firstName = document.querySelector('.first-name').value;
+    let lastName = document.querySelector('.last-name').value;
+    let email = document.querySelector('.email').value;
+    let formInfo = {
+        'firstName': firstName,
+        'lastName': lastName,
+        'email': email
     }
     return formInfo
 }
-document.querySelector('#mail-list-btn').addEventListener('click', function(){
-    let errors=document.querySelectorAll('.error')
-    for (let each of errors){
-        each.innerHTML=""
+let mailBtn = document.querySelector('.mail-list-btn')
+mailBtn.addEventListener('click', function () {
+    let errors = document.querySelectorAll('.error')
+    for (let each of errors) {
+        each.innerHTML = ""
     }
-    let form=extractMailFormValues()
-     console.log(form)
-    if(!form.firstName=='' && !form.lastName=='' && form.email.includes('@','.')){
-        document.querySelector('#mail-form').innerHTML="<h3 class='text-center'>Thanks for joining us!</h3>"
+    let form = extractMailFormValues()
+    if (!form.firstName == '' && !form.lastName == '' && form.email.includes('@', '.')) {
+        let mailForm = document.querySelector('.mail-form')
+        mailForm.innerHTML = "<h3 class='text-center'>Thanks for joining us!</h3>"
     }
-    else{
-        if (form.firstName==''){
-            document.querySelector('#first-name-error').innerHTML='Please enter your first name'
+    else {
+        if (form.firstName == '') {
+            document.querySelector('.first-name-error').innerHTML = 'Please enter your first name'
         }
-        if (form.lastName==''){
-            document.querySelector('#last-name-error').innerHTML='Please enter your last name'
-        }if (!form.email.includes('@','.')){
-            document.querySelector('#email-error').innerHTML='Please enter a valid email address'
+        if (form.lastName == '') {
+            document.querySelector('.last-name-error').innerHTML = 'Please enter your last name'
+        } if (!form.email.includes('@', '.')) {
+            document.querySelector('.email-error').innerHTML = 'Please enter a valid email address'
         }
     }
 })
+
+
+
